c90f15724565605baa4581fed5269b439a6ebcb4
sie07042 <sie07042@naver.com>
2026-03-14 17:07:29 +0900

    test

--- src/pages/DmPage.jsx
+++ src/pages/DmPage.jsx
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import GNB from '../components/Gnb'
 import Footer from '../components/Footer'
-import './DmPage.css'
 
 function DmPage() {
   const [rooms, setRooms] = useState([])
